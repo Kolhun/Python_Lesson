f8dcc6c08b7be0fc662d08b1be44dd1a5b18e279
Kolhun <36815965+Kolhun@users.noreply.github.com>
2024-06-05 20:24:52 +0300

    _22

---
+++ les_22.py
@@ -0,0 +1,19 @@
+class Car:
+    price = 1000000
+
+    def horse_powers(self):
+        return 100
+
+
+class Nissan(Car):
+    price = 800000
+
+    def horse_powers(self):
+        return 150
+
+
+class Kia(Car):
+    price = 600000
+
+    def horse_powers(self):
+        return 120
