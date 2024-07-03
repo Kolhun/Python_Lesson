77b60c3bffedbb76285e6ffdde5d5461790bd250
Kolhun <dim.prihodcko2020@yandex.ru>
2024-07-03 21:49:20 +0300

    les_32

---
+++ les_32.py
@@ -0,0 +1,21 @@
+class EvenNumbers:
+    def __init__(self, start=0, end=1):
+        self.start = start if start % 2 == 0 else start + 1
+        self.end = end if end % 2 == 0 else end - 1
+        self.current = self.start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.end:
+            raise StopIteration
+        else:
+            result = self.current
+            self.current += 2 
+            return result
+
+# Пример использования
+en = EvenNumbers(10, 25)
+for i in en:
+    print(i)
