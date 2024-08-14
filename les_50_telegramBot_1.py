5f2d8666c861d34eef8acb3fb2c9896c9ee71c62
Kolhun <36815965+Kolhun@users.noreply.github.com>
2024-08-14 17:51:29 +0300

    les_50_telegramBot_1

---
+++ les_50_telegramBot_1.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import asyncio
+import logging
+import sys
+import config
+
+api = config.TOKEN
+bot = Bot(token=api)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+
+@dp.message(CommandStart())
+async def start_message(message: Message):
+    await message.answer(f"Привет, <b>{message.from_user.full_name}</b>!", parse_mode="HTML")
+
+
+@dp.message()
+async def all_message(message: types.Message):
+    print(f"Мы получили сообщение - {message.text}")
+
+
+async def on_startup():
+    print("Bot is online!")
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    await on_startup()
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
